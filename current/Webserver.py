17e28c5ccb0c48355fbfab3286e5e15a401fd562
RachelSnow <rayt@ThinkPadT420>
2020-01-15 15:55:09 -0500

    static

--- current/Webserver.py
+++ current/Webserver.py
@@ -3,7 +3,7 @@
 import RPi.GPIO as GPIO
 import json
 from flask import Flask, render_template, request
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static' static_url_path='')
 
 P_DILDO = 12 # adapt to your wiring
 fPWM = 50  # Hz (not higher with software PWM)
